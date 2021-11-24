8ad8efa582806ad01bfefd63bc210e37a983a733
gvz42-web <gvz42-web@gmail.com>
2021-11-25 01:04:05 +0200

    Add simple layout

---
+++ components/Card.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="product-card">
+    <img src="https://via.placeholder.com/150" alt="" class="product-img">
+    <span class="product-title">Товар</span>
+    <span class="product-price">$12</span>
+    <button class="add-to-cart">Добавить в коризну</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class Card extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.product-card {
+  width: 200px;
+  height: 320px;
+  padding: 14px;
+  background-color: $light-gray;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 40px;
+
+  .product-img {
+
+  }
+
+  .product-title {
+    font-family: Raleway, 'sans-serif';
+    font-weight: 700;
+    font-size: 16px;
+  }
+
+  .product-price {
+    font-size: 22px;
+    align-self: flex-end;
+  }
+
+  .add-to-cart {
+    background-color: $primary;
+    outline: none;
+    border: none;
+    color: #fff;
+    height: 40px;
+    cursor: pointer;
+
+    &:hover {
+      background-color: $primary-darken;
+    }
+  }
+
+}
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,31 @@
 <template>
-  <Nuxt />
+  <div>
+    <Header />
+    <div class="container">
+      <Nuxt />
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import Header from '~/components/Header.vue'
+
+@Component({
+  // Declared as component data
+  components: {
+    Header
+  }
+})
+
+export default class defaultLayout extends Vue {}
+</script>
+
+<style lang="scss">
+  .container {
+    font-family: 'Open Sans',  Sans-Serif;
+    width: 1200px;
+    margin: $header-height+40px auto 0;
+  }
+</style>
