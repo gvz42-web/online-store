fa1a8e919f9bb7650e07bb01b1b24ed8b5d284fa
gvz42-web <gvz42-web@gmail.com>
2021-11-25 13:49:57 +0200

    Setup products store

---
+++ store/modules/product.js
@@ -0,0 +1,15 @@
+export const state = () => ({
+  products: []
+})
+
+export const getters = {
+  allPosts (state) {
+    return state.products
+  }
+}
+
+export const actions = {
+}
+
+export const mutations = {
+}
