519ead5f20c7674b24326394856511d6ddcf9b41
gvz42-web <gvz42-web@gmail.com>
2021-11-25 18:21:33 +0200

    Connect catalog with the cart store

--- store/product.js
+++ store/product.js
@@ -4,10 +4,8 @@ export const state = () => ({
 
 export const actions = {
   async fetchProducts ({ commit }) {
-    console.log('Fetch started')
     const res = await fetch('https://fakestoreapi.com/products/')
     const products = await res.json()
-    console.log('Fetch completed')
     commit('updateUsers', products)
   }
 }
@@ -15,13 +13,11 @@ export const actions = {
 export const mutations = {
   updateUsers (state, products) {
     state.products = products
-    console.log('Mutation completed', state)
   }
 }
 
 export const getters = {
   getProducts (state) {
-    console.log(state.products)
     return state.products
   }
 }
